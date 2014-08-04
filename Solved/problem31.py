93f6310dbf8e1643e389a01e3b26a2378c21ab29
StefanRvO <stefan@stefanrvo.dk>
2014-08-04 22:52:41 +0200

    Solved problem 31.

---
+++ Solved/problem31.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+ways=[]
+
+for i in xrange((200/1)+1):
+    print i
+    for j in xrange((200/2)+1):
+        if i+2*j>200:
+            break
+        for k in xrange((200/5)+1):
+            if i+2*j+5*k>200:
+                break
+            for l in xrange((200/10)+1):
+                if i+2*j+5*k+10*l>200:
+                    break
+                for m in xrange((200/20)+1):
+                    if i+2*j+5*k+10*l+20*m>200:
+                        break
+                    for n in xrange((200/50)+1):
+                        if i+2*j+5*k+10*l+20*m+50*n>200:
+                            break
+                        for o in xrange((200/100)+1):
+                            if i+2*j+5*k+10*l+20*m+50*n+100*o>200:
+                                break
+                            for p in xrange(2):
+                                if i+2*j+5*k+10*l+20*m+50*n+100*o+p*200==200:
+                                    ways.append([i,j,k,l,m,n,o,p])
+print len(ways)
+                                    
+                            
